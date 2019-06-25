19d3fa7f282baa5932fe7b233d18dcda36232224
Alan Zhong <alan.zhong.51@gmail.com>
2019-06-25 17:54:18 -0400

    implemented constructors for Queue

--- src/hw4q2.cpp
+++ src/hw4q2.cpp
@@ -1,4 +1,30 @@
+#ifndef __HW4Q2_CPP__
+#define __HW4Q2_CPP__
+
 #include "hw4q2.h"
 
+template <class Item>
+Queue<Item>::Queue() {
+    capacity = DEF_CAP;
+    used = 0;
+    first = 0;
+    last = 0;
+    data = new Item[capacity];
+}
 
+template <class Item>
+Queue<Item>::Queue(const Queue<Item>& source) {
+    capacity = source.capacity;
+    used = source.used;
+    first = source.first;
+    last = source.last;
+    for (int i = first; i < last; i = nextIndex(i)) {
+        data[i] = source.data[i];
+    }
+}
+
+template <class Item>
+Queue<Item>::~Queue() {
+    delete[] data;
+}
 
